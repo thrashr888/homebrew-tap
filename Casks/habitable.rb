b274d64be844bf32ef96ab543555e2e2cfb46128
Paul Thrasher <thrashr888@gmail.com>
2026-09-23 20:17:04 -0700

    Update Habitable cask to 0.3.0

--- Casks/habitable.rb
+++ Casks/habitable.rb
@@ -1,6 +1,6 @@
 cask "habitable" do
-  version "0.2.0"
-  sha256 "7e36c6c1a89470dc4460a63fd5d9395087d87a4e28054169674db29576f0b685"
+  version "0.3.0"
+  sha256 "067be909368dad8de81290627865a597202128ef98c93755f6a2734151f82d8c"
 
   url "https://github.com/thrashr888/homebrew-tap/releases/download/habitable-v#{version}/Habitable-#{version}-macos-universal.zip"
   name "Habitable"
