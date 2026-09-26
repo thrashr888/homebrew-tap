27d9514d5ba1f3e7135483b5baf02ed53570d82f
Paul Thrasher <pthrasher@hashicorp.com>
2026-09-25 18:03:30 -0700

    Update fan formula to v0.3.0

--- Formula/fan.rb
+++ Formula/fan.rb
@@ -1,8 +1,8 @@
 class Fan < Formula
   desc "Find why Mac fans are running using actual RPM and recent CPU activity"
   homepage "https://github.com/thrashr888/fan"
-  url "https://github.com/thrashr888/fan/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "472835379be6b28c8af14075d8a8ca5725d192cd776351787f53076d0c17a005"
+  url "https://github.com/thrashr888/fan/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "74c71cfefaa102340fe1af714b60b69e1a1b4514dd3490e020bd8bcdb7a274b3"
   license "MIT"
 
   depends_on :macos
